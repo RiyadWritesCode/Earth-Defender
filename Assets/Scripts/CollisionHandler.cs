32f54350257b88dcabfb4b09989475bc96e0fb34
Riyad270707 <contact.barbarygames@gmail.com>
2022-10-19 21:11:18 +0400

    Day 29
    Added particle explosions and stuff

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -6,13 +6,22 @@ using UnityEngine.SceneManagement;
 public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] float loadDelay = 1f;
+    [SerializeField] ParticleSystem playerCrash;
+    [SerializeField] GameObject leftCollider;
+    [SerializeField] GameObject mainCollider;
+    [SerializeField] GameObject rightCollider;
 
-    // void OnCollisionEnter(Collision other) {
-    //     Debug.Log($"{this.name} Collided with {other.gameObject.name}");
-    // }
 
     void OnTriggerEnter(Collider other) {
+        playerCrash.Play();
+        Destroy(leftCollider);
+        Destroy(mainCollider);
+        Destroy(rightCollider);
+        if (gameObject != null) 
+        {
         GetComponent<PlayerController>().enabled = false;
+        }
+        GetComponent<MeshRenderer>().enabled = false;
         Invoke("startCrashSequence", loadDelay);
     }
 
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] ParticleSystem crash;
+    [SerializeField] Transform parent;
+
     void OnParticleCollision(GameObject other)
     {
+        var vfx = Instantiate(crash, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent.transform;
         Destroy(gameObject);
     }
 }
