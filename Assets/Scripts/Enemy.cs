be3691a57e603f24c6354c0f349c6ca3e96baf35
Riyad270707 <contact.barbarygames@gmail.com>
2022-10-20 16:10:04 +0400

    Day 30

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,12 +5,39 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] ParticleSystem crash;
+    [SerializeField] ParticleSystem hit;
     [SerializeField] Transform parent;
+    [SerializeField] int hitpoints = 1;
+    ScoreBoard sb;
+
+    void Start()
+    {
+        sb = FindObjectOfType<ScoreBoard>();
+    }
 
     void OnParticleCollision(GameObject other)
     {
-        var vfx = Instantiate(crash, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent.transform;
-        Destroy(gameObject);
+        ProcessHit();
+        KillEnemy();
     }
+
+    void ProcessHit()
+    {
+        sb.IncreaseScore(15);
+    }
+
+    void KillEnemy()
+    {
+        hitpoints--;
+
+        var hitVFX = Instantiate(hit, transform.position, Quaternion.identity);
+        hitVFX.transform.parent = parent.transform;
+
+        if (hitpoints == 0)
+        {
+            var crashVFX = Instantiate(crash, transform.position, Quaternion.identity);
+            crashVFX.transform.parent = parent.transform;
+            Destroy(gameObject);
+        }       
+    }   
 }
